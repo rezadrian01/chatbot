6792c2afaeac7c524b9c7135c98521c5e8fb20fb
rezadrian01 <ahmadadrian324@gmail.com>
2024-08-08 19:50:17 +0700

    update ui chat

--- client/src/components/ChatArea.jsx
+++ client/src/components/ChatArea.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import {
   useActionData,
   json,
@@ -8,64 +8,90 @@ import {
 } from "react-router-dom";
 
 export default function ChatArea() {
-  //   console.log(import.meta.env.VITE_API_DOMAIN);
-  const [responses, setResponses] = useState([]);
-  const [prompts, setPrompts] = useState([]);
+  const [promptResponse, setPromptResponse] = useState({
+    prompts: [],
+    responses: [],
+  });
   const resultAction = useActionData();
   const submit = useSubmit();
+  const navigation = useNavigation();
+  const input = useRef();
   useEffect(() => {
     if (resultAction) {
-      setResponses((prevResponse) => {
-        const updatedResponses = [...prevResponse];
+      setPromptResponse((prevResponse) => {
+        const updatedResponses = [...prevResponse.responses];
         updatedResponses.push(resultAction.response);
-        return updatedResponses;
+        return {
+          ...prevResponse,
+          responses: updatedResponses,
+        };
       });
     }
   }, [resultAction]);
 
-  const navigation = useNavigation();
-  if (navigation.state === "submitting") {
-    return <p>Loading...</p>;
-  }
+  const submitting = navigation.state === "submitting";
   function handleSubmit(event) {
     event.preventDefault();
     const fd = new FormData(event.target);
     // const prompt = Object.fromEntries(fd.entries());
     const prompt = fd.get("prompt");
-    setPrompts((prevPrompts) => {
-      const updatedPrompts = [...prevPrompts];
+    setPromptResponse((prevPrompts) => {
+      const updatedPrompts = [...prevPrompts.prompts];
       updatedPrompts.push(prompt);
-      return updatedPrompts;
+      return {
+        ...prevPrompts,
+        prompts: updatedPrompts,
+      };
     });
+    input.current.value = "";
     submit(fd, { method: "POST" });
     return;
   }
 
   return (
     <>
-      <h1>ChatArea</h1>
-      <Form onSubmit={handleSubmit}>
-        <label>Prompt</label>
-        <input
-          className="bg-slate-100 px-3 py-1 rounded-full focus:outline-none"
-          placeholder="masukan prompt..."
-          type="text"
-          name="prompt"
-          id="prompt"
-        />
-        <button
-          className="bg-indigo-500 px-3 py-1 rounded text-slate-50"
-          type="submit"
-        >
-          Send
-        </button>
-      </Form>
-      <ul>
-        {responses &&
-          responses.map((response, index) => {
-            return <li key={index}>{response}</li>;
-          })}
-      </ul>
+      <h1 className="font-semibold text-xl">Chatbot</h1>
+      <div className="min-h-[93vh] overflow-y-auto flex flex-col justify-between pb-32 pt-6">
+        <ul className="flex flex-col gap-8">
+          {promptResponse.prompts &&
+            promptResponse.prompts.map((prompt, index) => {
+              return (
+                <li key={index} className="flex flex-col gap-8">
+                  <div className="text-right bg-slate-100 rounded-lg shadow p-6">
+                    <p>{prompt}</p>
+                  </div>
+                  {!promptResponse.responses[index] && submitting && (
+                    <p className="animate-pulse font-semibold">Loading...</p>
+                  )}
+                  {promptResponse.responses[index] && (
+                    <div className="text-left bg-slate-100 rounded-lg shadow p-6">
+                      <p>{promptResponse.responses[index]}</p>
+                    </div>
+                  )}
+                </li>
+              );
+            })}
+        </ul>
+        <Form className="relative" onSubmit={handleSubmit}>
+          <div className="fixed bottom-6 right-4 left-4 lg:right-36 lg:left-36 shadow-lg rounded-full">
+            <input
+              ref={input}
+              className="bg-zinc-100 px-3 py-1 rounded-full focus:outline-none w-full"
+              placeholder="Input some prompt..."
+              type="text"
+              name="prompt"
+              id="prompt"
+            />
+            <button
+              disabled={submitting}
+              className="absolute disabled:bg-indigo-600 disabled:text-stone-400 right-0 bg-indigo-500 hover:bg-indigo-600 px-3 py-1 rounded-e-full text-slate-50"
+              type="submit"
+            >
+              Send
+            </button>
+          </div>
+        </Form>
+      </div>
     </>
   );
 }
--- server/app.js
+++ server/app.js
@@ -13,8 +13,6 @@ const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 async function runGemini(prompt) {
-  //   const prompt =
-  //     "Tidak ada, jadi ini aku lagi membuat aplikasi chatbot dengan kamu gemini, dan ini lagi proses development";
   const result = await model.generateContent(prompt);
   const response = await result.response;
   const text = await response.text();
